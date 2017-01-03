62a3c22f09aa5a818060ae3565c8c863614b4ec4
Samuel Zamudio <szamudio15@hotmail.com>
2017-01-03 10:48:00 -0800

    first commit

---
+++ state1.js
@@ -0,0 +1,8 @@
+// Name of the game.
+var demo = {}; // empty object
+demo.state1 = function(){};
+demo.state1.prototype = {
+	preload: function(){},
+	create: function(){},
+	update: function(){}	
+};
